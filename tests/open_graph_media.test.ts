2d2c8355fd8778fda316c418af5f886632e9f761
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-25 00:41:41 -0400

    feat: add test for rendering open graph media information

---
+++ tests/open_graph_media.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+import { BASE_URL } from './constant';
+
+test('should render open graph media information correctly', async ({ page }) => {
+  await page.goto(`${BASE_URL}/open-graph-media-page`);
+  const ogImageWidth = await page.locator('meta[property="og:image:width"]').getAttribute('content');
+  const ogImageHeight = await page.locator('meta[property="og:image:height"]').getAttribute('content');
+  const ogImage = await page.locator('meta[property="og:image"]').getAttribute('content');
+  const ogSecureImage = await page.locator('meta[property="og:image:secure_url"]').getAttribute('content');
+  const ogImageType = await page.locator('meta[property="og:image:type"]').getAttribute('content');
+
+  expect(ogImageType).toBe('image/jpeg');
+  expect(ogImageWidth).toBe('1200');
+  expect(ogImageHeight).toBe('630');
+  expect(ogImage).toBe(`${BASE_URL}/image.jpg`);
+  expect(ogSecureImage).toBe(`${BASE_URL}/image.jpg`);
+});
