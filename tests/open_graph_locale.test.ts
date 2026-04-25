10a3f29cba044ccecbf1bb1718c8ee2ed855929b
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-25 00:41:30 -0400

    feat: add test for rendering locale information in Open Graph

---
+++ tests/open_graph_locale.test.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+import { BASE_URL } from './constant';
+
+test('should render locale information correctly', async ({ page }) => {
+  await page.goto(`${BASE_URL}/open-graph-locale-page`);
+
+  const ogLocale = await page.locator('meta[property="og:locale"]').getAttribute('content');
+  const ogLocaleAlternate = page.locator('meta[property="og:locale:alternate"]');
+  const count = await ogLocaleAlternate.count();
+  let ogLocaleAlternateContent = '';
+
+  expect(ogLocale).toBe('es');
+
+  for (let index = 0; index < count; index++) {
+    const localeContent = await ogLocaleAlternate.nth(index).getAttribute('content');
+    ogLocaleAlternateContent += localeContent + (index < count - 1 ? ', ' : '');
+  }
+  expect(ogLocaleAlternateContent).toBe('es, en, fr');
+});
