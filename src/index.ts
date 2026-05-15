4032465fb63721e7e2fbe86eb65cd10953d8f22c
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-14 22:32:13 -0400

    feat: add Typography component and type to index exports

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import type { RobotsProps } from './component/robots.astro';
 import type { RssProps } from './component/rss.astro';
 import type { SeoProps } from './component/seo.astro';
 import type { SitemapProps } from './component/sitemap.astro';
+import type { TypographyProps } from './component/typography.astro';
 
 export { default as Seo } from './component/seo.astro';
 export { default as Rss } from './component/rss.astro';
@@ -15,6 +16,7 @@ export { default as Robots } from './component/robots.astro';
 export { default as Favicon } from './component/favicon.astro';
 export { default as Sitemap } from './component/sitemap.astro';
 export { default as OpenGraph } from './component/open_graph.astro';
+export { default as Typography } from './component/typography.astro';
 export { default as MetaAuthor } from './component/meta_author.astro';
 export { default as GoogleAnalytic } from './component/google_analytic.astro';
 export { default as StructuredData } from './component/structured_data.astro';
@@ -28,6 +30,7 @@ export type {
   FaviconProps,
   SitemapProps,
   MetaAuthorProps,
+  TypographyProps,
   GoogleAnalyticProps,
   LanguageAlternativeProps
 };
